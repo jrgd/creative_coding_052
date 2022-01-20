1849114108bdec3342153d01c376193f358f0899
jrgd <jrgd@mac.mini.studio>
2022-01-19 22:56:06 -0500

    fix: single and double quotes in {query} passed to log via Alfred; fix also using the cli directly.

--- log.php
+++ log.php
@@ -1,19 +1,22 @@
-#!/usr/bin/php
+#!/usr/local/bin/php 
 <?php
 
 
 // week number and year
 $timestamp = time();
 $day_ = date("l");
+$day_n = date("d");
+$month_n = date("n");
 $week_n =  date("W");
 $year_n = date("Y");
 
-$file_name = "{$year_n}-{$week_n}-log.txt";
+// $file_name = "{$year_n}-{$week_n}-log.txt";
+$file_name = "activities-log.txt";
 $file_path = "/Users/jrgd/tools/logger/logs/{$file_name}";
 
 array_shift($argv); // pop out the first eleemnt of the array
-$arguments_string = implode(' ', $argv); // concatenation
-$text_log = "[{$timestamp}] {$arguments_string}\n";
+$arguments_string = implode(' ', $argv); // concatenation; also array_map('escapeshellarg', $argv)
+$text_log = "[{$timestamp} - {$year_n}-{$month_n}-{$day_n}] {$arguments_string}\n";
 
 echo $file_name.": ".$text_log;
 
